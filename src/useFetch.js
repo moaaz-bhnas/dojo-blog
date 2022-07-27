de144339e4650ebee70835a2d4979a9e5f29047e
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-07-27 13:52:11 +0200

    abort controller

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,25 +5,35 @@ export default function useFetch(url) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchData = useCallback(async () => {
-    try {
-      const res = await fetch(url);
-      if (!res.ok) {
-        throw Error("Could not fetch data for that resource");
-      }
+  const fetchData = useCallback(
+    async (abortController) => {
+      try {
+        const res = await fetch(url, { signal: abortController.signal });
+        if (!res.ok) {
+          throw Error("Could not fetch data for that resource");
+        }
 
-      const data = await res.json();
-      setData(data);
-      setError(null);
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-    }
-  }, [url]);
+        const data = await res.json();
+        setData(data);
+        setError(null);
+        setLoading(false);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          setError(err.message);
+          setLoading(false);
+        }
+      }
+    },
+    [url]
+  );
 
   useEffect(() => {
-    fetchData();
+    const abortController = new AbortController();
+    fetchData(abortController);
+
+    return () => abortController.abort();
   }, [url]);
 
   return { data, loading, error };
